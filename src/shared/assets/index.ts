d2a7d4eab1c2940789ceb7abc693c1abc92c225a
Dmitry Korovin <d.korovin@alt-it.solutions>
2023-10-28 06:50:20 +0300

    feat: add assets

---
+++ src/shared/assets/index.ts
@@ -0,0 +1,9 @@
+export { ReactComponent as FreudersLogo } from "./svg/freudersLogo.svg";
+export { ReactComponent as NoPhoto } from "./svg/noPhoto.svg";
+export { ReactComponent as NoPhotoMale } from "./svg/noPhotoMale.svg";
+export { ReactComponent as NoPhotoFemale } from "./svg/noPhotoFemale.svg";
+
+export { ReactComponent as BurgerIcon } from "./icons/burger.svg";
+export { ReactComponent as DotsIcon } from "./icons/dots.svg";
+export { ReactComponent as DropdownIcon } from "./icons/dropdown.svg";
+export { ReactComponent as EmptySearchIcon } from "./icons/emptySearch.svg";
